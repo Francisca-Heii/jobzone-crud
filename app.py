a3d11ce6f6495bbf2b7dadf5ea07eea1f4666e28
Francisca-Heii <franciscahheii@gmail.com>
2021-10-02 08:50:18 +0000

    removed errors in app.py

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
 import os
 from flask import Flask
-# from flask_pymongo import PyMongo
-# from bson.objectid import ObjectId
-
-
-if os.path.exists("env.py"):
-    import env
-
 
 app = Flask(__name__)
-
-app.config["MONGO_DBNAME"]=os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"]=os.environ.get("MONGO_URI")
-app.secret_key =os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -20,7 +9,7 @@ def hello():
     return "Hello world--again"
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True
